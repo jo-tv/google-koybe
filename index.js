29bec6d9b847b1fd4118362df669ab32fc4e3fda
josefuccef <josefuccef@gmail.com>
2025-08-23 18:22:37 +0100

    upd

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.post('/delete', requireAuth, async (req, res) => {
 // );
 
 // 🟢 الروابط المتاحة للبث
-const streamSources = ['https://mo3ad.xyz/live/gqG7SjCp59/NkwnxPayxd/'];
+const streamSources = ['https://mo3ad.xyz:443/mo3ad777a/mo3ad777a/'];
 
 // 🔹 مسار بث القناة
 app.get('/josef/stream/:channel', async (req, res) => {
